8f5526151b42f877d3f5951cfbc4a59577106039
sema0710 <pandati@naver.com>
2019-12-23 23:11:03 +0900

    [FIX]feedback 반영
    styled-component props에 대한 feedback 처리

--- src/components/Main/header/FollowRequest/FollowRequest.js
+++ src/components/Main/header/FollowRequest/FollowRequest.js
@@ -2,23 +2,29 @@ import React, { useState, useEffect } from 'react'
 import * as style from '../../../../style/TimeLineStyle'
 import * as img from '../../../../img'
 import { FollowRequestHeader } from './header'
-import { FollowRequestContent } from './content'
+// import { FollowRequestContent } from './content'
 
 function FollowRequest(){
 
-    const [ isClick,clickChange ] = useState("none");
+    const [ isClick,clickChange ] = useState(false);
     const [ deleteIndex, indexChange ] = useState(-1);
     const [ requestData, changeData ] = useState([]);
 
     const buttonClick = ()=>{
-        if(isClick === "none"){
-            clickChange("block");
+        if(!isClick){
+            clickChange(true);
         } else{
-            clickChange("none");
+            clickChange(false);
         }
     }
 
+
     useEffect(()=>{
+        console.log(isClick)
+    })
+
+    useEffect(()=>{
+        /* 수정해야 할 부분 */
         let data = [];
         if(deleteIndex >= 0){
             requestData.map((e)=>{
