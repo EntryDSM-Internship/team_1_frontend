ec57ff84d1a84575f5a5f3ec7bd097ca567c977a
sema0710 <pandati@naver.com>
2019-12-23 23:04:51 +0900

    [FIX]feedback 반영

--- src/components/mypage/profile/MypageUserInfo.js
+++ src/components/mypage/profile/MypageUserInfo.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as style from '../../../style/mypageStyle'
 
 function MypageUserInfo(){
 
-    let nickname = useRef();
-    let email = useRef();
+    const dummyData = {
+        email:"pandati@naver.com",
+        nickName:"sema0710"
+    }
+
+    const nickNameInput = useRef();
+    const emailInput = useRef();
+
+    const [ nickName, nickNameChange ] = useState("pandati");
+    const [ email , emailChange ] = useState("pandati@naver.com");
+
+    const valueChange = (value,stateFunc)=>{
+        stateFunc(value);
+    }
 
     useEffect(()=>{
-        nickname.current.value = "pandati"
-        email.current.value = "pandati@naver.com"
-    })
+        nickNameInput.current.value = dummyData.nickName;
+        emailInput.current.value = dummyData.email;
+    },[])
 
     return(
         <>
@@ -17,13 +29,13 @@ function MypageUserInfo(){
                 <style.MypageInfoTextDiv>
                     <style.MypageInfoText>닉네임</style.MypageInfoText>
                 </style.MypageInfoTextDiv>
-                <style.MypageInfoInput ref={nickname} type="text" placeholder="닉네임"/>
+                <style.MypageInfoInput ref={nickNameInput} onChange={(e)=>{valueChange(e.target.value,nickNameChange)}} type="text" placeholder="닉네임"/>
             </style.MypageInfoWrapper>
             <style.MypageInfoWrapper>
                 <style.MypageInfoTextDiv>
                     <style.MypageInfoText>사용자 이메일</style.MypageInfoText>
                 </style.MypageInfoTextDiv>
-                <style.MypageInfoInput ref={email} type="text" placeholder="이메일"/>
+                <style.MypageInfoInput ref={emailInput} onChange={(e)=>{valueChange(e.target.value,emailChange)}} type="text" placeholder="이메일"/>
             </style.MypageInfoWrapper>
         </>
     );
