4f93a6338b5b11850c652000212e2dd1d3a14465
sema0710 <pandati@naver.com>
2019-12-31 09:06:30 +0900

    followerview 서버통신 구현
    서버 통신 구현 후 follow 삭제는 안함

--- src/pages/FollowerView.js
+++ src/pages/FollowerView.js
@@ -1,24 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as style from '../style/FollowerViewStyle'
 import { Header } from '../components/Main/header'
 import { FollowerViewContent } from '../components/FollowerView'
+import axios from 'axios'
 
-function FollowerView(){
+function FollowerView({access_token,tokenChange,refreshAccessToken}){
+    const [isFollow,followChange] = useState(true);
+    const [content,contentChange] = useState([]);
+    
+    useEffect(()=>{
+        refreshAccessToken()
+        .then((req)=>{
+            setContent(isFollow ? getFollower : getFollowing);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    },[])
 
-    const [ isCheck,checkChange ] = useState({"follower": false , "follow":true});
+    useEffect(()=>{
+        setContent(isFollow ? getFollower : getFollowing);
+    },[isFollow])
+
+
+    const getFollower = ()=> new Promise(function(resolve,reject){
+        axios.get("http://10.156.147.200:3000/api/follow/followlist",{
+            headers:{
+                "x-access-token" : access_token,
+            }
+        }).then((req)=>{
+            const follower = req.data.follower;
+            console.log(req)
+            resolve(follower);
+        }).catch((err)=>{
+            reject(err);
+        })
+    })
+
+    const getFollowing = ()=>{
+        return new Promise(function(resolve,reject){
+            axios.get("http://10.156.147.200:3000/api/follow/followlist",{
+                headers:{
+                    "x-access-token" : access_token,
+                }
+            }).then((req)=>{
+                const following = req.data.following;
+                console.log(following)
+                resolve(following);
+            }).catch((err)=>{
+                reject(err);
+            })
+        })
+    }
+
+    const setContent = (dataFunc)=>{
+        dataFunc()
+        .then((data)=>{
+            let content = [];
+            data.map((e)=>{
+                content.push(<FollowerViewContent name={e.nick} userImg={e.profile} follow={isFollow}></FollowerViewContent>);
+            })
+            contentChange(content);
+        })
+    }
 
     return(
         <>
-            <Header></Header>
+            <Header access_token={access_token}/>
             <style.FollowerView>
                 <style.FollowerViewMain>
                     <style.FollowerViewChoiceButtonDiv>
-                        <style.FollowerViewChoiceButton onClick={()=>{checkChange({"follower": true , "follow":false})}} click={isCheck.follower}>팔로워</style.FollowerViewChoiceButton>
-                        <style.FollowerViewChoiceButton onClick={()=>{checkChange({"follower": false , "follow":true})}} click={isCheck.follow}>팔로우</style.FollowerViewChoiceButton>
+                        <style.FollowerViewChoiceButton onClick={()=>{followChange(true)}} click={isFollow}>팔로워</style.FollowerViewChoiceButton>
+                        <style.FollowerViewChoiceButton onClick={()=>{followChange(false)}} click={!isFollow}>팔로우</style.FollowerViewChoiceButton>
                     </style.FollowerViewChoiceButtonDiv>
-                    <style.FollowerViewContents>
-                        <FollowerViewContent></FollowerViewContent>
-                    </style.FollowerViewContents>
+                    <style.FollowerViewWrapper>
+                        <style.FollowerViewContents>
+                            {content ? content : "리스트가 없습니다"}
+                        </style.FollowerViewContents>
+                    </style.FollowerViewWrapper>
                 </style.FollowerViewMain>
             </style.FollowerView>
         </>
