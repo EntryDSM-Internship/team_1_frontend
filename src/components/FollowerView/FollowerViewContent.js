b2a4b886ba1024a52d6bbbc1fc1283d056a3b74d
sema0710 <pandati@naver.com>
2019-12-31 09:15:08 +0900

    followerView 서버통신 2

--- src/components/FollowerView/FollowerViewContent.js
+++ src/components/FollowerView/FollowerViewContent.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as style from '../../style/FollowerViewStyle'
 
-function FollowViewContent(){
+
+function FollowViewContent({name,follow,userImg}){
+
+    // const followingCancel = ()=>{
+    //     axios({
+    //         method:"DELETE",
+    //         header:{
+    //             "x-access-token":accessToken,
+    //         }
+    //         url:""
+    //     })
+    // }
+
     return(
         <>
             <style.FollowerViewContentDiv>
                 <style.FollowerViewSide display="space-between">
-                    <style.FollowerViewImg src="https://t1.daumcdn.net/cfile/tistory/99954A3F5CDEA26A1B"/>  
-                    <style.FollowerViewUserName>오준상</style.FollowerViewUserName>
+                    <style.FollowerViewImg alt="유저 이미지" src={`http://10.156.147.200:3000${userImg ? userImg : "/profile/0.jpg"}`}/>  
+                    <style.FollowerViewUserName>{name}</style.FollowerViewUserName>
                 </style.FollowerViewSide>
                 <style.FollowerViewSide display="center">
-                    <style.FollowerViewButton>팔로잉</style.FollowerViewButton>
+                <style.FollowerViewButton>{follow ? "팔로잉" : "팔로워"}</style.FollowerViewButton>
                 </style.FollowerViewSide>
             </style.FollowerViewContentDiv>
         </>
